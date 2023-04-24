745eb707d623421df5e22fb2f51c2a5c30253fbf
Keith Brings <keith.brings@noizu.com>
2023-04-25 02:39:32 +0700

    mix tweak

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule NoizuLabsTesting.MixProject do
   def project do
     [
       app: :noizu_labs_testing,
+      name: "NoizuLabs Testing",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
@@ -14,6 +15,7 @@ defmodule NoizuLabsTesting.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:noizu_labs_testing],
       extra_applications: [:logger]
     ]
   end
